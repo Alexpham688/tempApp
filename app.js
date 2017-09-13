c67f6277afed755457f33edfc16281223a740885
Alexpham688 <Alexpham688@gmail.com>
2017-09-13 09:38:06 -0400

    add things

--- app.js
+++ app.js
@@ -7,20 +7,20 @@ $(document).ready(function(){
   $(search).on("focus", function() {
     $(this).animate({
       width: "100%"
-    }, 400);
+    }, 500);
     $(button).animate({
     right: '10px'
-  }, 400);
+  }, 500);
   });
   //blur event
   $(search).on("blur", function() {
     if(search.val() === "") {
       $(search).animate({
       width: "45%"
-    }, 400, function(){
+    }, 500, function(){
       $(button).animate({
         right: "360px"
-      }, 400, function(){});
+      }, 500, function(){});
 
     });
     }
@@ -33,7 +33,7 @@ $(document).ready(function(){
     async: false,
     success: function(data){
       console.log(data.main.temp);
-      $('#minn-temp').html(data.main.temp + ' &#176' + " C");
+      $('#minn-temp').html(data.main.temp * (9/5) + 32 + ' &#176' + " F");
       $('#minn-humid').html(data.main.humidity + " &#37")
     }
   });
@@ -43,7 +43,7 @@ $(document).ready(function(){
   $('button').on("click", function(){
       var location = $('#location').val();
       $("#location").val("");
-      $("#show-weather").html("The city you chose is " + location + ".");
+      $("#show-weather").html("Temperature & Humidity for " + location + ".");
         $.ajax({
           url:'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&units=metric&APPID=3f196ebe1a8f3f884f4a32af14171ea6',
           type:"GET",
@@ -51,12 +51,13 @@ $(document).ready(function(){
           async: true,
           success: function(data){
             console.log(data.main.temp);
-            $("#main-temp").html(data.main.temp + ' &#176' + " C");
-            $('#main-humidity').html(data.main.humidity + " &#37");
+            $("#main-temp").html(data.main.temp * (9/5) + 32 + ' &#176' + " F");
+            $('#main-humidity').html(data.main.humidity  + " &#37");
           },
           error: function(){
-              $('#error').html("ERROR!");
-          }
+              $('#error').html("Enter a city!");
+              $('#error').reset();
+              }
         });
 });
 });
